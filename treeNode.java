ac6bdd95cb3994891f4d22fac1759f4378fde59e
Alex Edwards <edwardsar65@gmail.com>
2015-04-19 09:25:42 -0700

    treeNode class for use in avl tree

---
+++ treeNode.java
@@ -0,0 +1,84 @@
+/* Alexander Edwards
+*  
+*  
+*/
+public class treeNode< T > {
+// ==== hasLeft() ====
+	public boolean hasLeft(){
+		if(leftNode == null){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+// ==== hasRight() ====
+	public boolean hasRight(){
+		if(rightNode == null){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+// **** Getters ****
+
+// ==== T getData() ====
+	public T getData(){
+		return this.data;
+	}
+// ==== getLeftNode() ====
+	public treeNode<T> getLeftNode(){
+		return this.leftNode;
+	}
+// ==== getRightNode() ====
+	public treeNode<T> getRightNode(){
+		return	this.rightNode;
+	}
+// ==== getHeight() ====
+	public int getHeight(){
+		return this.height;
+	}
+
+// **** Setters ****
+
+// ==== setData(T data) ====
+	public void setData( T data){
+		this.data = data;
+	}
+// ==== setLeftNode() ====
+	public void setLeftNode(treeNode<T> left){
+		this.leftNode = left;
+	}
+// ==== setRightNode() ====
+	public void setRightNode(treeNode<T> right){
+		this.rightNode = right;
+	}
+// ==== setHeight() ====
+	public void setHeight(int h){
+		this.height = h;
+	}
+
+// ==== constructors ====
+	public treeNode(T data){
+		this(data,null,null);
+	}
+	public treeNode(T data, treeNode<T> left, treeNode<T> right){
+		this.data = data;
+		this.leftNode = left;
+		this.rightNode = right;
+		this.height = 0;
+	}
+	public treeNode(treeNode<T> sourceOfCopy ){
+		this.data = sourceOfCopy.data;
+		this.leftNode = sourceOfCopy.leftNode;
+		this.rightNode = sourceOfCopy.rightNode;
+		this.height = sourceOfCopy.height;
+	}
+
+// **** data members ****
+	private int height;
+	private T data;
+	private treeNode< T > leftNode;
+	private treeNode< T > rightNode;
+}
