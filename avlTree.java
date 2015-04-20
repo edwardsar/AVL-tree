306fd4210d46836b944d975efa72e6b88d9ece8d
Alex Edwards <edwardsar65@gmail.com>
2015-04-20 02:04:52 -0700

    implemented some insert, print, traversal functionality

--- avlTree.java
+++ avlTree.java
@@ -3,7 +3,7 @@
 *
 */
 
-public class avlTree< T >{
+public class avlTree< T extends Comparable<T> >{
 // ==== Java function pointer equivalent ====
 	public interface FunctionPointer<F> {
 		public void execute(F data);
@@ -19,13 +19,52 @@ public class avlTree< T >{
 			inOrderTraversal(current.getRightNode(),fptr);
 		}
 	}
+// ==== insert() ====
+// Note to self: java passes by value, even references.
+	private void insertRecursiveHelper(T data, treeNode<T> current){
+		if(current.getData().compareTo(data) == 0){       // case: equals
+			return;
+		}
+		else if(current.getData().compareTo(data) > 0 ){ // case: less than
+			if(current.hasLeft()){
+				insertRecursiveHelper(data, current.getLeftNode());
+			}
+			else{
+				current.setLeftNode(new treeNode<T>(data));
+				size++;
+			}
+		}
+		else if(current.getData().compareTo(data) < 0){ // case: greater than
+			if(current.hasRight()){
+				insertRecursiveHelper(data, current.getRightNode());
+			}
+			else{
+				current.setRightNode(new treeNode<T>(data));
+				size++;
+			}
+		}
+	}
+	public void insert(T data){
+		if(root == null){
+			root = new treeNode<T>(data);
+			if(root == null){
+				fullFlag = true;
+				return;
+			}
+			size++;
+		}
+		else{
+			insertRecursiveHelper(data, root);
+		}
+	}
 // ==== print() ====
 	public void print(){
 		inOrderTraversal(root,new FunctionPointer<T>(){
 									public void execute(T data){
-										System.out.print(data.toString());
+										System.out.print(data.toString() + ", ");
 									}
 		} );
+		System.out.println();
 	}
 	
 // ==== constructors ====
