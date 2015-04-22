21cf533e1850b068b2b9000a7e5d77ba2030478e
Alex Edwards <edwardsar65@gmail.com>
2015-04-21 20:47:12 -0700

    fixed insert bug(I was using compareTo() incorrectly) and implemented avl tree hierarchy printing

--- avlTree.java
+++ avlTree.java
@@ -19,22 +19,24 @@ public class avlTree< T extends Comparable<T> >{
 			inOrderTraversal(current.getRightNode(),fptr);
 		}
 	}
-// ==== insert() ====
-// Note to self: java passes by value, even references.
-	private void insertRecursiveHelper(T data, treeNode<T> current){
-		if(current.getData().compareTo(data) == 0){       // case: equals
+// ==== reverseOrderTraversal() ====
+	private void reverseOrderTraversal(treeNode<T> current, FunctionPointer<T> fptr){
+		if(current == null){
 			return;
 		}
-		else if(current.getData().compareTo(data) > 0 ){ // case: less than
-			if(current.hasLeft()){
-				insertRecursiveHelper(data, current.getLeftNode());
-			}
-			else{
-				current.setLeftNode(new treeNode<T>(data));
-				size++;
-			}
+		else{
+			reverseOrderTraversal(current.getRightNode(),fptr);
+			fptr.execute(current.getData());
+			reverseOrderTraversal(current.getLeftNode(),fptr);
 		}
-		else if(current.getData().compareTo(data) < 0){ // case: greater than
+	}
+// ==== insert() ====
+// Note to self: java passes by value, even references. No manipulating pointer in recursion.
+	private void insertRecursiveHelper(T data, treeNode<T> current){
+		if(current.getData().compareTo(data) == 0){       // case: obj1 equals obj2
+			return;
+		}
+		else if(current.getData().compareTo(data) < 0 ){ // case: obj1 less than obj2 
 			if(current.hasRight()){
 				insertRecursiveHelper(data, current.getRightNode());
 			}
@@ -43,22 +45,27 @@ public class avlTree< T extends Comparable<T> >{
 				size++;
 			}
 		}
+		else if(current.getData().compareTo(data) > 0){ // case: obj1 greater than obj2
+			if(current.hasLeft()){
+				insertRecursiveHelper(data, current.getLeftNode());
+			}
+			else{
+				current.setLeftNode(new treeNode<T>(data));
+				size++;
+			}
+		}
 	}
 	public void insert(T data){
 		if(root == null){
 			root = new treeNode<T>(data);
-			if(root == null){
-				fullFlag = true;
-				return;
-			}
 			size++;
 		}
 		else{
 			insertRecursiveHelper(data, root);
 		}
 	}
-// ==== print() ====
-	public void print(){
+// ==== printInOrder() ====
+	public void printInOrder(){
 		inOrderTraversal(root,new FunctionPointer<T>(){
 									public void execute(T data){
 										System.out.print(data.toString() + ", ");
@@ -66,7 +73,36 @@ public class avlTree< T extends Comparable<T> >{
 		} );
 		System.out.println();
 	}
-	
+// ==== printReverseOrder() ====
+	public void printReverseOrder(){
+		reverseOrderTraversal(root,new FunctionPointer<T>(){
+									public void execute(T data){
+										System.out.print(data.toString() + ", ");
+									}
+		} );
+		System.out.println();	
+	}
+// ==== printHierarchy() ====
+	private void printHierarchyRecursiveHelper(String formatingString, treeNode<T> current){
+		System.out.println(current.getData().toString());		
+		if(current.hasLeft()){
+			System.out.print(formatingString );
+			System.out.print("|~~");
+			printHierarchyRecursiveHelper( formatingString + "|  ", current.getLeftNode());
+		}
+		if(current.hasRight()){
+			System.out.print(formatingString );
+			System.out.print("|~~");
+			printHierarchyRecursiveHelper( formatingString + "   ", current.getRightNode());
+			System.out.println(formatingString);
+		}
+		
+	}
+	public void printHierarchy(){
+		if( root != null){
+			printHierarchyRecursiveHelper("",root);
+		}
+	}
 // ==== constructors ====
 	public avlTree(){
 		this.root = null;
