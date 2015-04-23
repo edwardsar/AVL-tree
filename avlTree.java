227ca797da3c8fcea006a1ac31789f3444ea69be
Alex Edwards <edwardsar65@gmail.com>
2015-04-23 01:32:17 -0700

    Inserting is most likely correct. Inserting produces correct tree structures for the tests so far.

--- avlTree.java
+++ avlTree.java
@@ -32,7 +32,7 @@ public class avlTree< T extends Comparable<T> >{
 	}
 
 // ==== insert() ====
-// Note to self: java passes by value, even references. No manipulating pointer in recursion.
+// Note to self: java passes by value, even references. No directly manipulating pointer in recursion.
 	private treeNode<T> insertRecursiveHelper(T data, treeNode<T> currentNode){
 		if(currentNode.getData().compareTo(data) == 0){       // case: obj1 equals obj2
 			return currentNode;
@@ -40,65 +40,66 @@ public class avlTree< T extends Comparable<T> >{
 		else if(currentNode.getData().compareTo(data) < 0 ){ // case: obj1 less than obj2 
 			if(currentNode.hasRight()){
 				currentNode.setRightNode( insertRecursiveHelper(data, currentNode.getRightNode()) );
+				currentNode.incrementHeightByOne();
 			}
 			else{
 				currentNode.setRightNode(new treeNode<T>(data));
 				size++;
 			}
-			currentNode.incrementHeightByOne();
 		}
 		else if(currentNode.getData().compareTo(data) > 0){ // case: obj1 greater than obj2
 			if(currentNode.hasLeft()){
 				currentNode.setLeftNode( insertRecursiveHelper(data, currentNode.getLeftNode()) );
+				currentNode.incrementHeightByOne();
 			}
 			else{
 				currentNode.setLeftNode(new treeNode<T>(data));
 				size++;
-			}
-			currentNode.decrementHeightByOne();
+			}		
 		}
-
-		// ---- check for balance ----
-		if(currentNode.getHeight() <= -2){ // heavy on the left
-			treeNode<T> tempNode = currentNode.getLeftNode();
-			// **** right rotation ****
-			if(tempNode.getHeight() == -1){
-				tempNode.setRightNode(currentNode);
-				currentNode.setLeftNode(null);
-				currentNode.zeroHeight();
-				tempNode.zeroHeight();
-			}
-			// **** right left rotation ****
-			else if( tempNode.getHeight() == 1){
-				tempNode.setLeftNode(tempNode.getRightNode());
-				tempNode.setRightNode(currentNode);
-				currentNode.setLeftNode(null);
-				currentNode.zeroHeight();
-				tempNode.zeroHeight();
-			}
-
-			return tempNode;
+ 		//if height of children differ by more than 1 rotate
+		int balanceFactor = currentNode.getLeftsHeight() - currentNode.getRightsHeight();
+		if(balanceFactor == 2){
+				// rotate right
+				if(!currentNode.getLeftNode().hasRight()){
+					treeNode<T> tempNode = currentNode.getLeftNode();
+					tempNode.setRightNode(currentNode);
+					currentNode.setLeftNode(null);
+					currentNode.computeNewHeight();
+					tempNode.computeNewHeight();
+					return tempNode;
+				}
+				// double rotate right
+				else{
+					treeNode<T> tempNode = currentNode.getLeftNode();
+					currentNode.setLeftNode(tempNode.getRightNode());
+					tempNode.setRightNode(currentNode);
+					currentNode.computeNewHeight();
+					tempNode.computeNewHeight();
+					return tempNode;
+				}
 		}
-		else if(currentNode.getHeight() >= 2){ // heavy on the right
-			treeNode<T> tempNode = currentNode.getRightNode();
-			// **** left rotation ****
-			if(tempNode.getHeight() == 1){
-				tempNode.setLeftNode(currentNode);
-				currentNode.setRightNode(null);
-				currentNode.zeroHeight();
-				tempNode.zeroHeight();
-			}
-			// **** left right rotation ****
-			else if(tempNode.getHeight() == -1){
-				tempNode.setRightNode(tempNode.getLeftNode());
-				tempNode.setLeftNode(currentNode);
-				currentNode.setRightNode(null);
-				currentNode.zeroHeight();
-				tempNode.zeroHeight();
-			}
-			return tempNode;
+		if(balanceFactor == -2){
+				// rotate left
+				if(!currentNode.getRightNode().hasLeft()){
+					treeNode<T> tempNode = currentNode.getRightNode();
+					tempNode.setLeftNode(currentNode);
+					currentNode.setRightNode(null);
+					currentNode.computeNewHeight();
+					tempNode.computeNewHeight();
+					return tempNode;
+				}
+				// double rotate left
+				else{
+					treeNode<T> tempNode = currentNode.getRightNode();
+					currentNode.setRightNode(tempNode.getLeftNode());
+					tempNode.setLeftNode(currentNode);
+					currentNode.computeNewHeight();
+					tempNode.computeNewHeight();
+					return tempNode;
+				}
 		}
-		
+		currentNode.computeNewHeight();
 		return currentNode;
 	}
 	public void insert(T data){
@@ -110,23 +111,7 @@ public class avlTree< T extends Comparable<T> >{
 			root = insertRecursiveHelper(data, root);
 		}
 	}
-// ==== calcHeight() ====
-	private int calcHeight(treeNode<T> left, treeNode<T> right){
-		int x, y;
-		if(left != null){
-			x = left.getHeight();
-		}
-		else{
-			x = -1;
-		}
-		if(right != null){
-			y = right.getHeight();
-		}
-		else{
-			y = -1;
-		}
-		return x - y;
-	}
+
 // ==== printInOrder() ====
 	public void printInOrder(){
 		inOrderTraversal(root,new FunctionPointer<T>(){
@@ -145,9 +130,14 @@ public class avlTree< T extends Comparable<T> >{
 		} );
 		System.out.println();	
 	}
-// ==== printHierarchy() ====
+// ==== printHierarchy()  && helper====
+	public void printHierarchy(){
+		if( root != null){
+			printHierarchyRecursiveHelper("",root);
+		}
+	}
 	private void printHierarchyRecursiveHelper(String formatingString, treeNode<T> currentNode){
-		System.out.println(currentNode.getData().toString() + " :H-> " + currentNode.getHeight());		
+		System.out.println(currentNode.getData().toString());		
 		if(currentNode.hasLeft()){
 			System.out.println(formatingString + "|");
 			System.out.print(formatingString );
@@ -163,11 +153,7 @@ public class avlTree< T extends Comparable<T> >{
 		
 		
 	}
-	public void printHierarchy(){
-		if( root != null){
-			printHierarchyRecursiveHelper("",root);
-		}
-	}
+
 // ==== constructors ====
 	public avlTree(){
 		this.root = null;
